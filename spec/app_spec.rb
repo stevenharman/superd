e2e93896c1cfbe9e16d33be99868acf8c2e4f704
Steven Harman <steveharman@gmail.com>
2011-07-12 19:17:53 -0400

    clean up specs & prepare for 404

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -5,18 +5,25 @@ describe 'superd' do
   describe 'looking at root' do
     before { get '/' }
 
-    specify { last_response.should be_successful }
-    specify { last_response.body.should include("Hello, Worlds!") }
+    it { last_response.should be_successful }
+    it { last_response.body.should include("Hello, Worlds!") }
   end
 
-  describe 'looking at a specific poster', :type => :request do
+  describe 'looking at a specific poster' do
 
-    context 'that exists' do
+    context 'that exists', :type => :request do
       before { visit '/foo-bar' }
 
       it 'show the poster' do
-        page.should have_selector('.poster img')
+        page.should have_selector(".poster img[src='foo-bar.png']")
       end
     end
+
+    context "that doesn't exist" do
+      before { get '/no-such-poster' }
+
+      xit { last_response.status.should be 404 }
+    end
+
   end
 end
