7ca40be359af916f9dcd093b81394f801ee01c7c
Steven Harman <steveharman@gmail.com>
2011-07-12 12:34:33 -0400

    teach Compass about production env

--- app.rb
+++ app.rb
@@ -7,6 +7,7 @@ configure do
   Compass.configuration do |config|
     config.project_path = File.dirname(__FILE__)
     config.sass_dir = 'views/stylesheets/'
+    config.environment = :production if ENV['RACK_ENV'] == 'production'
   end
 
   set :scss, Compass.sass_engine_options
