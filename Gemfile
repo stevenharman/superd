7811d3e9fd661c2485fa1c546e4e6053b626b99f
Steven Harman <steveharman@gmail.com>
2011-07-12 12:18:25 -0400

    compass in the house

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source :rubygems
 
+gem 'compass', ">= 0.11.5" #must be loaded before sinatra
 gem 'sinatra', '1.2.6'
 gem 'thin', '~> 1.2.11'
 
@@ -7,4 +8,3 @@ group :development, :test do
   gem 'heroku'
   gem 'foreman'
 end
-
--- app.rb
+++ app.rb
@@ -1,5 +1,21 @@
 require 'sinatra'
 require 'erb'
+require 'sass'
+require 'compass'
+
+configure do
+  Compass.configuration do |config|
+    config.project_path = File.dirname(__FILE__)
+    config.sass_dir = 'views/stylesheets/'
+  end
+
+  set :scss, Compass.sass_engine_options
+end
+
+get '/styles.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  scss :'stylesheets/styles'
+end
 
 get '/' do
   erb :index
