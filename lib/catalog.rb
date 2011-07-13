3f296fb1c69d10d036253c6d4d3b9c7fab23eb1d
Steven Harman <steveharman@gmail.com>
2011-07-13 01:50:21 -0400

    catalog is initalized with an array of posters

--- lib/catalog.rb
+++ lib/catalog.rb
@@ -1,11 +1,11 @@
 class Catalog
   attr_reader :posters
 
-  def initialize()
-    @posters = Dir.glob("public/images/posters/*")
+  def initialize(posters=[])
+    @posters = posters
   end
 
   def find_by_name(name)
-    posters.find { |p| File.basename(p, '.*') == name.downcase }
+    posters.find { |p| p.name.downcase == name.downcase }
   end
 end
--- spec/catalog_spec.rb
+++ spec/catalog_spec.rb
@@ -1,23 +1,25 @@
 require_relative '../lib/catalog.rb'
+require_relative '../lib/poster.rb'
 
 describe Catalog do
 
-  context 'a new catalog' do
+  context 'with posters' do
     before do
-      Dir.stub(:glob).and_return(['public/images/posters/foo.png', 'public/images/posters/bar.jpg', 'public/images/posters/baz.gif'])
-      @catalog = Catalog.new
-    end
-
-    it 'populates itself with posters' do
-      @catalog.posters.should have(3).items
+      #Dir.stub(:glob).and_return(['public/images/posters/foo.png', 'public/images/posters/bar.jpg', 'public/images/posters/baz.gif'])
+      posters = %w(foo.png bar.gif BAZ.jpg).collect { |f| Poster.new("images/posters/#{f}") }
+      @catalog = Catalog.new(posters)
     end
 
     it 'can retrieve a poster by name' do
       @catalog.find_by_name('bar').should_not be_nil
     end
 
-    it 'can retrieve a poster by name regardless of case' do
+    it 'can retrieve a poster by name with mixed case' do
       @catalog.find_by_name('BaR').should_not be_nil
+    end
+
+    it 'can retrieve a poster by name with wrong case' do
+      @catalog.find_by_name('baz').should_not be_nil
     end
 
     it 'returns nil for non-existant posters' do
